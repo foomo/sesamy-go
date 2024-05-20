e9853efff8cbaa166d137c38487274a6e97ac70f
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-05-20 17:46:19 +0200

    feat: make event name required

--- event.go
+++ event.go
@@ -2,7 +2,7 @@ package sesamy
 
 type Event[P any] struct {
 	// Reserved names: https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_names
-	Name EventName `json:"name,omitempty"`
+	Name EventName `json:"name"`
 	// Reserved parameter names: https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_parameter_names
 	Params P `json:"params,omitempty"`
 }
