199b141f1fd2ea32be761baeb10a5a1ddd608301
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-08-22 12:24:15 +0200

    feat: use items

--- pkg/provider/emarsys/event/params/emarsysview.go
+++ pkg/provider/emarsys/event/params/emarsysview.go
@@ -1,6 +1,6 @@
 package params
 
 type EmarsysView[I any] struct {
-	Item       I      `json:"item,omitempty"`
+	Items      []I    `json:"sitem,omitempty"`
 	PageViewID string `json:"page_view_id,omitempty" dlv:"emarsys.page_view_id"`
 }
