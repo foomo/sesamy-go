a44e4b1e4c899860a8244e3881d7d51a1843a141
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-08-22 12:27:11 +0200

    fix: typo

--- pkg/provider/emarsys/event/params/emarsysview.go
+++ pkg/provider/emarsys/event/params/emarsysview.go
@@ -1,6 +1,6 @@
 package params
 
 type EmarsysView[I any] struct {
-	Items      []I    `json:"sitem,omitempty"`
+	Items      []I    `json:"items,omitempty"`
 	PageViewID string `json:"page_view_id,omitempty" dlv:"emarsys.page_view_id"`
 }
