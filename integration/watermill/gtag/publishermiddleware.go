d289d042982a2017f8cddf43e8bc1e98f7aefa2b
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-11-12 10:31:30 +0100

    fix: only log on ignore

--- integration/watermill/gtag/publishermiddleware.go
+++ integration/watermill/gtag/publishermiddleware.go
@@ -2,13 +2,18 @@ package gtag
 
 import (
 	"github.com/ThreeDotsLabs/watermill/message"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 func PublisherMiddlewareIgnoreError(next PublisherHandler) PublisherHandler {
 	return func(l *zap.Logger, msg *message.Message) error {
-		err := next(l, msg)
-		l.With(zap.Error(err)).Warn("ignoring error")
+		if err := next(l, msg); err != nil {
+			if spanCtx := trace.SpanContextFromContext(msg.Context()); spanCtx.IsValid() && spanCtx.IsSampled() {
+				l = l.With(zap.String("trace_id", spanCtx.TraceID().String()), zap.String("span_id", spanCtx.SpanID().String()))
+			}
+			l.With(zap.Error(err)).Warn("ignoring error")
+		}
 		return nil
 	}
 }
--- integration/watermill/mpv2/publishermiddleware.go
+++ integration/watermill/mpv2/publishermiddleware.go
@@ -5,14 +5,19 @@ import (
 
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/foomo/sesamy-go/pkg/encoding/mpv2"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 // PublisherMiddlewareIgnoreError ignores error responses from the gtm endpoint to prevent retries.
 func PublisherMiddlewareIgnoreError(next PublisherHandler) PublisherHandler {
 	return func(l *zap.Logger, msg *message.Message) error {
-		err := next(l, msg)
-		l.With(zap.Error(err)).Warn("ignoring error")
+		if err := next(l, msg); err != nil {
+			if spanCtx := trace.SpanContextFromContext(msg.Context()); spanCtx.IsValid() && spanCtx.IsSampled() {
+				l = l.With(zap.String("trace_id", spanCtx.TraceID().String()), zap.String("span_id", spanCtx.SpanID().String()))
+			}
+			l.With(zap.Error(err)).Warn("ignoring error")
+		}
 		return nil
 	}
 }
