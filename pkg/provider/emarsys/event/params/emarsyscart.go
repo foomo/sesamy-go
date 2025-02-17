54545c6e06564e0b9950b04ea0554602b7072b86
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2025-02-17 09:44:46 +0100

    revert(emarsys): use event names

--- pkg/provider/emarsys/event/params/emarsyscart.go
+++ pkg/provider/emarsys/event/params/emarsyscart.go
@@ -1,6 +1,6 @@
 package params
 
-type EmarsysPageView[I any] struct {
+type EmarsysCart[I any] struct {
 	Items      []I    `json:"items,omitempty" dlv:"dataModel.items"`
 	PageViewID string `json:"page_view_id,omitempty" dlv:"emarsys.page_view_id"`
 }
--- pkg/provider/emarsys/event/params/emarsysviewitem.go
+++
@@ -1,6 +0,0 @@
-package params
-
-type EmarsysViewItem[I any] struct {
-	Items      []I    `json:"items,omitempty" dlv:"dataModel.items"`
-	PageViewID string `json:"page_view_id,omitempty" dlv:"emarsys.page_view_id"`
-}
