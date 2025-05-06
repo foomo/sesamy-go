8b3a763010f0293b77e555fcded9f4ad93cc8032
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2025-05-06 23:54:17 +0200

    fix: support gs1 and gs2 session parsing

--- pkg/session/ga.go
+++ pkg/session/ga.go
@@ -2,9 +2,18 @@ package session
 
 import (
 	"net/http"
-	"strings"
+	"regexp"
 
 	"github.com/pkg/errors"
+)
+
+// see https://www.bbccss.com/explanation-of-cookie-values-used-by-ga4.html/comment-page-1#comment-6684
+var (
+	GA1Regex = regexp.MustCompile(`^GA1\.(\d+)\.(\d+)\.(\d+)$`)
+	// GS1.<domain_level>.<session_id>.<session_count>.<engagement_session>.<timestamp>.<countdown>.<enhanced_client_id>
+	GS1Regex = regexp.MustCompile(`^GS1\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)\.(\d+)$`)
+	// GS2.<domain_level>.s<session_id>$o<session_count>$g<engagement_session>$t<timestamp>$j<countdown>$l<undefined>$h<enhanced_client_id>
+	GS2Regex = regexp.MustCompile(`^GS2\.(\d+)\.s(\d+)\$o(\d+)\$g(\d+)\$t(\d+)\$j(\d+)\$l(\d+)\$h(\d+)$`)
 )
 
 func ParseGAClientID(r *http.Request) (string, error) {
@@ -13,12 +22,12 @@ func ParseGAClientID(r *http.Request) (string, error) {
 		return "", errors.Wrap(err, "failed to retrieve _ga cookie")
 	}
 
-	parts := strings.Split(cookie.Value, ".")
-
 	// validate
-	if !strings.HasPrefix(cookie.Value, "GA1.1") || len(parts) < 4 {
+	if !GA1Regex.MatchString(cookie.Value) {
 		return "", errors.New("invalid _ga cookie value")
 	}
+
+	parts := GA1Regex.FindStringSubmatch(cookie.Value)
 
 	return parts[2] + "." + parts[3], nil
 }
@@ -29,14 +38,14 @@ func ParseGASessionID(r *http.Request, id string) (string, error) {
 		return "", errors.Wrap(err, "failed to retrieve _ga cookie")
 	}
 
-	parts := strings.Split(cookie.Value, ".")
-
-	// validate
-	if !strings.HasPrefix(cookie.Value, "GS1.1") || len(parts) < 3 {
-		return "", errors.New("invalid _ga cookie value")
+	switch {
+	case GS1Regex.MatchString(cookie.Value):
+		return GS1Regex.FindStringSubmatch(cookie.Value)[2], nil
+	case GS2Regex.MatchString(cookie.Value):
+		return GS2Regex.FindStringSubmatch(cookie.Value)[2], nil
+	default:
+		return "", errors.Wrap(errors.New("invalid _ga cookie value"), cookie.Value)
 	}
-
-	return parts[2], nil
 }
 
 func ParseGASessionNumber(r *http.Request, id string) (string, error) {
@@ -45,12 +54,12 @@ func ParseGASessionNumber(r *http.Request, id string) (string, error) {
 		return "", errors.Wrap(err, "failed to retrieve _ga cookie")
 	}
 
-	parts := strings.Split(cookie.Value, ".")
-
-	// validate
-	if !strings.HasPrefix(cookie.Value, "GS1.1") || len(parts) < 4 {
-		return "", errors.New("invalid _ga cookie value")
+	switch {
+	case GS1Regex.MatchString(cookie.Value):
+		return GS1Regex.FindStringSubmatch(cookie.Value)[3], nil
+	case GS2Regex.MatchString(cookie.Value):
+		return GS2Regex.FindStringSubmatch(cookie.Value)[3], nil
+	default:
+		return "", errors.Wrap(errors.New("invalid _ga cookie value"), cookie.Value)
 	}
-
-	return parts[3], nil
 }
---
+++ pkg/session/ga_test.go
@@ -0,0 +1,61 @@
+package session_test
+
+import (
+	"testing"
+
+	"github.com/foomo/sesamy-go/pkg/session"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGARegex(t *testing.T) {
+	ga1 := "GA1.1.584335997.1746564151"
+
+	{
+		require.True(t, session.GA1Regex.MatchString(ga1))
+		matches := session.GA1Regex.FindStringSubmatch(ga1)
+		require.Len(t, matches, 4)
+		assert.Equal(t, "1", matches[1])
+		assert.Equal(t, "584335997", matches[2])
+		assert.Equal(t, "1746564151", matches[3])
+	}
+}
+
+func TestGSRegex(t *testing.T) {
+	gs1 := "GS1.1.1746524658.33.0.1746524658.0.0.0"
+	gs2 := "GS2.1.s1746524658$o1$g0$t1746562809$j0$l0$h596732783"
+
+	{
+		require.True(t, session.GS1Regex.MatchString(gs1))
+		matches := session.GS1Regex.FindStringSubmatch(gs1)
+		require.Len(t, matches, 9)
+		assert.Equal(t, "1", matches[1])
+		assert.Equal(t, "1746524658", matches[2])
+		assert.Equal(t, "33", matches[3])
+		assert.Equal(t, "0", matches[4])
+		assert.Equal(t, "1746524658", matches[5])
+		assert.Equal(t, "0", matches[6])
+		assert.Equal(t, "0", matches[7])
+		assert.Equal(t, "0", matches[8])
+
+		assert.False(t, session.GS1Regex.MatchString(gs2))
+		assert.Empty(t, session.GS1Regex.FindStringSubmatch(gs2))
+	}
+
+	{
+		require.True(t, session.GS2Regex.MatchString(gs2))
+		matches := session.GS2Regex.FindStringSubmatch(gs2)
+		require.Len(t, matches, 9)
+		assert.Equal(t, "1", matches[1])
+		assert.Equal(t, "1746524658", matches[2])
+		assert.Equal(t, "1", matches[3])
+		assert.Equal(t, "0", matches[4])
+		assert.Equal(t, "1746562809", matches[5])
+		assert.Equal(t, "0", matches[6])
+		assert.Equal(t, "0", matches[7])
+		assert.Equal(t, "596732783", matches[8])
+
+		assert.False(t, session.GS2Regex.MatchString(gs1))
+		assert.Empty(t, session.GS2Regex.FindStringSubmatch(gs1))
+	}
+}
